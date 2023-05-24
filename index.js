14af25d6ebd02dcca7984b2778675e9bd2d43e1b
Luiz F. Picolo <gugarbravin@gmail.com>
2023-05-24 14:09:12 -0400

    tarefa ltp

---
+++ index.js
@@ -0,0 +1,27 @@
+class Pessoa {
+    constructor(nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+mostrar(){
+    try{
+        return this.atributos();
+    } 
+    catch(erro){
+     console.log(erro.stack.message)
+    }
+    
+}
+
+    atributos(){
+        if(this.nome != "" && this.sobrenome != ""){
+            return this.nome + this.sobrenome
+        }
+        else{
+      throw new Error("Ta errado, ta faltando nome ou sobrenome")
+        }
+    }
+}
+
+const pessoa = new Pessoa("", "Picolo");
+console.log(pessoa.mostrar())
